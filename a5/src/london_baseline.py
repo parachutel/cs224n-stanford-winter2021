63f38618646fc96a327697c3226b08924aa5d4fc
Sheng Li <mrlouisleel@gmail.com>
2021-02-14 13:46:30 -0800

    update baseline

--- a5/src/london_baseline.py
+++ a5/src/london_baseline.py
@@ -2,3 +2,21 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+import argparse
+
+argp = argparse.ArgumentParser()
+argp.add_argument('--eval_corpus_path',
+    help="Path of the corpus to evaluate on", default=None)
+argp.add_argument('--outputs_path', default=None)
+args = argp.parse_args()
+
+predictions = [] 
+for line in tqdm(open(args.eval_corpus_path)):
+	predictions.append('London')
+total, correct = utils.evaluate_places(args.eval_corpus_path, predictions)
+
+if total > 0:
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+else:
+    print('Predictions written to {}; no targets provided'.format(args.outputs_path))
