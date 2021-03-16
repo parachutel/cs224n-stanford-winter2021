534ac110e7f43893132e446fd44ad8c326eb1e7e
Sheng Li <mrlouisleel@gmail.com>
2021-03-16 15:40:46 -0700

    update

--- squad/analysis.py
+++ squad/analysis.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def analysis(gold_dict, pred_dict):
     fig, ax = plt.subplots()
+    count_mat = np.zeros((26, 26))
     for key, value in pred_dict.items():
         if len(gold_dict[key]['answers']) > 0:
             ground_truth = gold_dict[key]['answers'][0].split(' ')
@@ -11,8 +13,14 @@ def analysis(gold_dict, pred_dict):
             prediction = value.split(' ')
         else:
             prediction = ''
-        # print(ground_truth, len(ground_truth), '|', prediction, len(prediction))
-        ax.scatter(len(ground_truth), len(prediction), color='red', s=2)
+        
+        if len(ground_truth) <= 25 and len(prediction) <= 25:
+            count_mat[len(ground_truth), len(prediction)] += 1
+
+    count_mat = np.flipud(count_mat)
+    plt.imshow(count_mat)
+    plt.gca().invert_yaxis()
+    plt.colorbar()
     ax.plot([0, 25], [0, 25], color='black')
     ax.set_xlim(0, 25)
     ax.set_ylim(0, 25)
