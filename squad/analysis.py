e715b6b4d7e446b63349b3534cb741f669c7b4d1
Sheng Li <mrlouisleel@gmail.com>
2021-03-16 15:05:35 -0700

    fix

--- squad/analysis.py
+++ squad/analysis.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 def analysis(gold_dict, pred_dict):
     for key, value in pred_dict.items():
         if len(gold_dict[key]['answers']) > 0:
-            ground_truth = gold_dict[key]['answers'][0]
+            ground_truth = gold_dict[key]['answers'][0].split(' ')
         else:
             ground_truth = ''
-        prediction = value
+        prediction = value.split(' ')
         plt.scatter(len(ground_truth), len(prediction), color='red')
+    plt.xlabel('Ground Truth Length')
+    plt.ylabel('Prediction Length')
     plt.savefig('./length_analysis.png')
