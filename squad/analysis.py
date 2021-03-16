f66658bd7747eee6fd1a530153791c1fa6fe43c7
Sheng Li <mrlouisleel@gmail.com>
2021-03-16 16:11:03 -0700

    fix

--- squad/analysis.py
+++ squad/analysis.py
@@ -19,15 +19,26 @@ def analysis(gold_dict, pred_dict):
 
     plot_x_list = []
     plot_y_list = []
+    plot_bar_list = []
     for l in sorted(count_dict):
         if len(count_dict[l]) > 0:
+            plot_bar_list.append(len(count_dict[l]))
             plot_x_list.append(l)
             plot_y_list.append(np.mean(count_dict[l]))
-    ax.plot([0, 25], [0, 25], color='black')
+
+    ax2 = ax.twinx()
+    ax2.set_ylabel('Ground Truth Length Freq.')  # we already handled the x-label with ax1
+    ax2.bar(plot_x_list, plot_bar_list)
+
+    ax.plot([0, 25], [0, 25], linestyle='dashed', color='black', label='Accuracy Reference')
     ax.plot(plot_x_list, plot_y_list, color='red')
     ax.set_xlim(0, 25)
     ax.set_ylim(0, 25)
     ax.set_xlabel('Ground Truth Length')
-    ax.set_ylabel('Prediction Length')
+    ax.set_ylabel('Mean Prediction Length')
     ax.set_aspect('equal', 'box')
+
+    
+
+    plt.legend()
     fig.savefig('./length_analysis.png')
